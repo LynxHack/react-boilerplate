151a4a26a45634a28238f15f9dcde034195e19a8
klynx <KevinLynx_L@hotmail.com>
2018-07-10 18:23:55 -0700

    Added image recognition capability to 60% max width of screen

--- src/Message.jsx
+++ src/Message.jsx
@@ -8,10 +8,19 @@ class Message extends Component {
     }
   }
   
+  createpost(content){
+    const last3char = content.substr(content.length - 3)
+    if( last3char === 'jpg' || last3char === 'png' || last3char === 'gif'){
+      return (<span className="message-content"><img src={content} style={{maxWidth : '60%'}}/></span>)
+    }
+    return (<span className="message-content">{content}</span>) 
+  }
+
   render(){
     return(<div className="message">
           <span className="message-username" style={{color: this.props.color}}>{this.props.username}</span>
-          <span className="message-content">{this.props.content}</span>
+          {/* <span className="message-content">{this.props.content}</span> */}
+          {this.createpost(this.props.content)}
         </div>);
   }
 }
